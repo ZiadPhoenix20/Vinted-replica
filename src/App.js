31fecfbb4d99e573039960bc2c26c5bcfdb57d9b
Ziad zaoui <ziadpro@MBP-de-Ziad.mshome.net>
2020-11-11 15:20:25 +0100

    header creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./containers/Home";
 
 function App() {
-  return <div>Hello from</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>logo</p>
+      <button>s'incrire</button>
+      <button>se connecter</button>
+      <button>vends tes articles</button>
+    </div>
+  );
+};
+
+export default Header;
